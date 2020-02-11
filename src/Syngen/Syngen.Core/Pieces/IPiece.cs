a5cc643eb8e6330be548706b8e70858a742df267
Wieger Hinderks <wiegerhinderks@gmail.com>
2020-02-11 20:17:13 +0100

    Added more summaries

--- src/Syngen/Syngen.Core/Pieces/IPiece.cs
+++ src/Syngen/Syngen.Core/Pieces/IPiece.cs
@@ -7,8 +7,19 @@ namespace Syngen.Core.Pieces
     /// </summary>
     public interface IPiece : IColourable
     {
+        /// <summary>
+        /// Abbreviation for the chess notation.
+        /// </summary>
         char Abbreviation { get; }
+
+        /// <summary>
+        /// Name of the piece.
+        /// </summary>
         string Name { get; }
+
+        /// <summary>
+        /// Value of the piece.
+        /// </summary>
         int Value { get; }
     }
 }
